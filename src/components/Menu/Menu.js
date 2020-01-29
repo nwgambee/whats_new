9fb90175b73c5500c22ed1bf785678a85cf23da6
Noah Gambee <noah@gambee.dev>
2020-01-28 17:38:03 -0700

    Add name attribute to buttons in Menu comp.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,11 +6,11 @@ import './Menu.css'
 const Menu = ({changeSource}) => {
   return (
     <div className='Menu'>
-      <button className='news-src-btn' onClick={changeSource}>Entertainment</button>
-      <button className='news-src-btn' onClick={changeSource}>Health</button>
-      <button className='news-src-btn' onClick={changeSource}>Local</button>
-      <button className='news-src-btn' onClick={changeSource}>Science</button>
-      <button className='news-src-btn' onClick={changeSource}>Technology</button>
+      <button className='news-src-btn' onClick={changeSource} name='entertainment'>Entertainment</button>
+      <button className='news-src-btn' onClick={changeSource} name='health'>Health</button>
+      <button className='news-src-btn' onClick={changeSource} name='local'>Local</button>
+      <button className='news-src-btn' onClick={changeSource} name='science'>Science</button>
+      <button className='news-src-btn' onClick={changeSource} name='technology'>Technology</button>
     </div>
   )
 }
