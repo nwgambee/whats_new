b1b6d4885b57f056bca697df615a3dabb700f761
Noah Gambee <noah@gambee.dev>
2020-01-29 18:01:25 -0700

    PAss snapshpt test for NewsArticle

--- src/components/NewsArticle/NewsArticle.test.js
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -4,8 +4,13 @@ import { shallow } from 'enzyme';
 import NewsArticle from './NewsArticle';
 
 describe('NewsArticle', () => {
-  it('', () => {
-    const wrapper = shallow(<    />);
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<NewsArticle
+        img='some file'
+        headline='Denver Man Eats Bug'
+        description='A denver man has eaten a bug'
+        url='some url'
+          />);
 
     expect(wrapper).toMatchSnapshot();
   });
